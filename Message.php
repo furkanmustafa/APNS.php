070dd4c9a44b2543a4423a2f28020566d2b5a78f
Furkan Mustafa <furkan@s2n.io>
2014-12-21 15:04:04 +0900

    All in one.  - Autoloadable classes  - New protocol support to be able to send bigger payloads (up to 2KB instead of 256bytes)

---
+++ Message.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace FMAPNS;
+use \Exception;
+
+class Message {
+  
+  const  POWER_SAVING         = 5;
+  const  IMMEDIATE            = 10;
+  
+  public $token               = null;
+  
+  public $alert               = null;
+  public $sound               = null;
+  public $badge               = null;
+  
+  public $localizedAlertKey   = null;
+  public $localizedActionKey  = null;
+  public $localizedAlertArgs  = null;
+  public $launchImage         = null;
+  
+  public $userinfo            = null;
+  
+  public $priority            = self::IMMEDIATE;
+  public $expiresAt           = 0; // unixtime, 0 = never expires
+  public $identifier          = 0; // will be set to a random number, override if you want to track status
+  
+  function __construct($token = null) {
+    static $randomSequence = 0;
+    if ($randomSequence == 0)
+      $randomSequence = mt_rand();
+    
+    $this->token = $token;
+    $this->identifier = ++$randomSequence;
+  }
+  function to($token) {
+    $copy = clone $this;
+    $copy->token = $token;
+    return $copy;
+  }
+
+  function data() {
+    $body = [
+      'aps' => []
+    ];
+
+    if ($this->alert && !($this->localizedAlertArgs || $this->localizedAlertKey || $this->localizedActionKey || $this->launchImage)) {
+      $body['aps']['alert'] = $this->alert;
+    } else if ($this->localizedAlertArgs || $this->localizedAlertKey || $this->localizedActionKey || $this->launchImage) {
+      if ($this->localizedAlertKey)
+        $_alert['loc-key'] = $this->localizedAlertKey;
+      if ($this->localizedAlertArgs)
+        $_alert['loc-args'] = $this->localizedAlertArgs;
+      if ($this->localizedActionKey)
+        $_alert['action-loc-key'] = $this->localizedActionKey;
+      if ($this->launchImage)
+        $_alert['launch-image'] = $this->launchImage;
+      if ($this->alert)
+        $_alert['body'] = $this->alert;
+  
+      $body['aps']['alert'] = $_alert;
+    }
+
+    if ($this->sound !== null) {
+      $body['aps']['sound'] = $this->sound;
+    }
+
+    if ($this->badge !== null) {
+      $body['aps']['badge'] = $this->badge;
+    }
+
+    if ($this->userinfo && is_array($this->userinfo))
+      $body = array_merge($body, $this->userinfo);
+
+    return $body;
+  }
+  
+  function json() {
+    return json_encode($this->data());
+  }
+  
+  function payload() {
+    
+    $payload = $this->json();
+    
+    // Old Protocol
+    
+    // Build the binary notification
+    // $msg = chr(0) . pack('n', 32);
+    // if (strlen($this->token) == 32) {
+    //   $msg .= $this->token;
+    // } else if (strlen($this->token) == 64) {
+    //   $msg .= pack('H*', $this->token);
+    // } else {
+    //   throw new Exception('Invalid Token');
+    // }
+    // $msg .= pack('n', strlen($payload)) . $payload;
+    // if (strlen($msg) > 255) {
+    //   throw new Exception('Payload too big');
+    // }
+    
+    // New Protocol
+    
+    $frame = '';
+    // prepare the frame
+    // 1. token
+    $item = pack('H*', $this->token);
+    $frame .= chr(1) . pack('n', strlen($item)) . $item;
+
+    // 2. payload
+    if (strlen($payload) > 2048)
+      throw new Exception('Payload too big');
+    
+    $item = $payload;
+    $frame .= chr(2) . pack('n', strlen($item)) . $item;
+
+    // 3. identifier (4 bytes) = 0?
+    $item = pack('N', $this->identifier);
+    $frame .= chr(3) . pack('n', strlen($item)) . $item;
+
+    // 4. expiration (4 bytes) = 0
+    $item = pack('N', $this->expiresAt);
+    $frame .= chr(4) . pack('n', strlen($item)) . $item;
+
+    // 5. priority ( 5 = power-saving, 10 = immediate )
+    $item = chr($this->priority);
+    $frame .= chr(5) . pack('n', strlen($item)) . $item;
+    
+    // Pack the frame
+    $msg = chr(2) . pack('N', strlen($frame)) . $frame;
+    
+    return $msg;
+  }
+  
+  function send() {
+    Connection::SendMessage($this); // Using the default connection
+  }
+  function sendTo($otherToken) {
+    Connection::SendMessage($this->to($otherToken)); // Using the default connection
+  }
+}
--- README.md
+++ README.md
@@ -3,27 +3,45 @@ APNS.php
 
 Simple APNS Client for php. Works. Doesn't have 3589454 files and abstract classes in it.
 
+*It now supports up to 2048bytes payloads for iOS8+*
+
+As documented here: https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/CommunicatingWIthAPS.html
+
 ```php
-require_once('APNSMessage.php');
-require_once('APNS.php');
+require_once('FMAPNS/Connection.php');
+require_once('FMAPNS/Message.php');
 
 APNS::$Defaults['certificateFile'] = 'push-prod.pem';
 APNS::$Defaults['certificatePass'] = 'mycertificatepasswordifiprotecteditwithapasswordatall';
+// APNS::$Defaults['stage'] = APNS::DEVELOPMENT; // Default is PRODUCTION
 
 $message = new APNSMessage('49aaa3feb1dfe5f7fa91a0e9bedddddda760a54c08abaa376a9b30006dec2ccc');
 $message->alert = "Hello Computer!";
 $message->badge = 2;
 $message->sound = 'default';
+// Pass extra metadata in `userinfo` property of message
+$message->userinfo['screen_id'] = 25;
 
 echo "Sending..\n";
-APNS::Connection([ 'stage' => APNS::PRODUCTION ])->send($message);
-APNS::Connection([ 'stage' => APNS::PRODUCTION ])->close();
+APNS\Connection::Shared()->send($message);
+APNS\Connection::Shared()->close();
 echo "Done!\n";
 ```
 
 You usually won't need it (in linux), but in case you need it;
 
 ```php
-// I've downloaded it from: https://www.entrust.net/downloads/binary/entrust_2048_ca.cer
 APNS::$Defaults['caFile'] = 'entrust_2048_ca.cer';
+// I've downloaded it from: https://www.entrust.net/downloads/binary/entrust_2048_ca.cer
 ```
+
+TODO
+====
+
+ - [ ] Feedback Service poller
+ - [ ] Cleanup Message class and move protocol related stuff into Connection class
+
+LICENSE
+=======
+
+See [LICENSE](LICENSE)
